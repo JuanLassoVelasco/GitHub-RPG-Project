edb575dba98addf3952c5ef4195b229638ee4680
Juan Lasso Velasco <juanlassovelasco@gmail.com>
2020-06-17 17:56:13 -0400

    Made Sandbox Level and set up navMesh

---
+++ Assets/Scripts/PlayerNavigation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerNavigation : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+
+    NavMeshAgent playerNav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerNav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+        {
+            playerNav.SetDestination(target.position);
+        }
+    }
+}
