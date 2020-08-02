281ae4cc4bdc42a10b8bfdcb4396ecb0d0f9eb6f
Juan Lasso Velasco <juanlassovelasco@gmail.com>
2020-08-02 13:33:46 -0400

    pre push commit

--- Assets/Scripts/Movement/PlayerNavigation.cs
+++ Assets/Scripts/Movement/PlayerNavigation.cs
@@ -17,7 +17,7 @@ public class PlayerNavigation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
